98c1a33e2a81649a785fe97e8e1c3398e2b2dbca
Musthak <musthakcool55@gmail.com>
2026-06-16 16:39:48 +0530

    Add admin config, forms, and AUTH_USER_MODEL setting

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+
+    model = CustomUser
+
+    list_display = (
+        "id",
+        "email",
+        "username",
+        "user_type",
+        "phone_number",
+        "is_staff",
+        "is_active",
+    )
+
+    list_filter = (
+        "user_type",
+        "is_staff",
+        "is_active",
+    )
+
+    search_fields = (
+        "email",
+        "username",
+        "phone_number",
+    )
+
+    ordering = (
+        "id",
+    )
+
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal Info", {"fields": ("phone_number", "user_type")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "username",
+                "email",
+                "user_type",
+                "phone_number",
+                "password1",
+                "password2",
+                "is_staff",
+                "is_active",
+            ),
+        }),
+    )
---
+++ accounts/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+
+        fields = (
+            "username",
+            "email",
+            "user_type",
+            "phone_number",
+            "password1",
+            "password2",
+        )
+
+
+class CustomUserUpdateForm(UserChangeForm):
+
+    password = None
+
+    class Meta:
+        model = CustomUser
+
+        fields = (
+            "username",
+            "email",
+            "phone_number",
+        )
